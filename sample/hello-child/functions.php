70ec0fe1bb24617765d0dc336887cc60f00d0ef1
r-matsuzaka <matsuzakaryo@gmail.com>
2022-06-08 23:22:09 +0900

    feat:remove sample

--- sample/hello-child/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function hello_child_enqueue_child_styles() {
-	$parent_style = 'hello-stylesheet'; 
-	wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version') );
-	}
-add_action( 'wp_enqueue_scripts', 'hello_child_enqueue_child_styles' );
--- sample/hello/footer.php
+++
@@ -1,5 +0,0 @@
-	    </div>
-	</div>
-<?php wp_footer(); ?>
-</body>
-</html>
